5a43a01cfdf711122e07bb3793a0284189a61cff
Kevin Cole <kevin.cole@outlook.com>
2021-09-18 16:05:41 -0600

    Update Program.cs

--- ACT.ScreenGrab/ACT.ScreenGrab.Console/Program.cs
+++ ACT.ScreenGrab/ACT.ScreenGrab.Console/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using OpenQA.Selenium;
 using System.Drawing;
 using System.Drawing.Imaging;
-using OpenQA.Selenium.Remote;
 using System.IO;
 using System.Diagnostics;
 
